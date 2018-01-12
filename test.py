8f4a9a064ad2b9e0a393c68efa4230d78ec58ec6
ManifoldFR <wilson.jallet@gmail.com>
2018-01-12 17:57:41 +0100

    Amélioration implémentation
    La définition des fonctions éléments finis P1 et leurs dérivées ont été
    simplifiées.
    
    Les matrices de masse et de rigidité ont été renommées de manière
    adéquate.
    
    Ajout d'un module pour faire le graphe de la solution et du second
    membre

--- test.py
+++ test.py
@@ -4,95 +4,35 @@
 
 # Implémentations et tests numériques
 
-import matplotlib.pyplot as p
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numba
 import numpy as np
 from scipy.integrate import quad
-from scipy import misc
 
-# Définitions des paramétres 
-n = 10
-m = 10  # méthide des points fixes
-P = 1
-I = 10
+print(mpl.use('Qt4Agg'))
 
-# choix des fonction f1 et f2 pour le choix de f sous forme séparée
-print "----------------------------------------------------------"
-def f1(x):
-	return x**2
-def f2(y):
-	return y**2
+gridsize = 20 # taille I de la grille
+h = 1./gridsize # Pas de discrétisation de [0,1]
 
-
-h=1./I # Pas de discrétisation de l'intervalle [0,1]
-
-
-les_phi = []  # liste qui contiendra fonctions phi i
-
-def phi_0(x):
-	if(x >=0 and x <= h):
-		return 1. - x/h
+# Définition des éléments finis P1
+def phi(x,i=None):
+	if i is None:
+		return np.maximum(0,1-np.absolute(x))
 	else:
-		return 0
-les_phi.append(phi_0)
+		return phi((x-i*h)/h)
 
-for i in range(1,I):
-
-	def phi_i(x):
-		if(x >= (i-1)*h and x <= (i+1)*h):
-			if(x <= i*h):
-				return 1. - (i*h - x)/h
-			else:
-				return 1. - (x - i*h)/h
-			
-		else :
-			return 0.
-
-	les_phi.append(phi_i)
-
-	
-def phi_I(x):
-	if(x >= (I-1)*h and x <= 1):
-		return 1. + (x - I*h)/h
+def dPhi(x,i):
+	if(x >= (i-1)*h and x <= (i+1)*h):
+		if(x <= i*h):
+			return 1./h
+		else:
+			return -1./h
 	else :
-		return 0
-les_phi.append(phi_I)
+		return 0.
 
-
-
-les_phi_derivate = []	  # Liste qui contiendra les dérivées des fonction phi i 
-
-def phi_0_derivate(x):
-	if(x >=0 and x <= h):
-		return -1./h
-	else:
-		return 0
-les_phi_derivate.append(phi_0_derivate)
-
-for i in range(1,I):
-
-	def phi_i_derivate(x):
-		if(x >= (i-1)*h and x <= (i+1)*h):
-			if(x <= i*h):
-				return 1./h
-			else:
-				return -1./h
-			
-		else :
-			return 0.
-
-	les_phi_derivate.append(phi_i_derivate)
-
-def phi_I(x):
-	if(x >= (I-1)*h and x <= 1):
-		return 1./h
-	else :
-		return 0
-les_phi_derivate.append(phi_i_derivate)
-
-
-
-
-def quad_trapz(g,a,b,p = 5):      # Calcucl de l'intégrale de g entre a et b
+def quad_trapz(g,a,b,p = 10):      # Calcul de l'intégrale de g entre a et b
 
     step = (b-a)/(p+1)
     first_term = 0.5*(g(a)+g(b))
@@ -100,99 +40,144 @@ def quad_trapz(g,a,b,p = 5):      # Calcucl de l'intégrale de g entre a et b
     return step*(first_term + second_term)
 
 
-D_ = []  # arrays des matrices D et M définies dans le problème
-M_ = []
+# matrices de rigidité D et de masse M définies dans le problème
+def build_matrices():
+	print("\nComputing mass and rigidity matrices...")
 
-for i in range(I+1):
-	D_.append([])
-	M_.append([])
+	rigidity_matrix = np.zeros((gridsize+1,gridsize+1))
+	mass_matrix = np.zeros((gridsize+1,gridsize+1))
 
-for i in range(I+1):
-	for j in range(I+1):
-		def f(x):
-			fi = les_phi_derivate[i]
-			fj = les_phi_derivate[j]
-			return fi(x) * fj(x)
-		def g(x):
-			gi = les_phi[i]
-			gj = les_phi[j]
-			return gi(x) * gj(x)
+	for i in range(gridsize+1):
+		for j in range(gridsize+1):
+			rigidity_matrix[i,j] = quad_trapz(
+				lambda x: dPhi(x,i)*dPhi(x,j),0,1,15)
+			mass_matrix[i,j] = quad_trapz(
+				lambda x: phi(x,i)*phi(x,j),0,1,15)
 
-		D_[i].append(quad_trapz(f,0,1,p=5))
-		M_[i].append(quad_trapz(g,0,1,p=5))
-		
+	print(np.array_repr(mass_matrix,max_line_width=160,precision=4))
+	return mass_matrix,rigidity_matrix
 
+mass_matrix,rigidity_matrix = build_matrices()
 
+print("\nComputing tensorial representation of RHS function...")
 
-D = np.array(D_)  # Matrices D et M
-M = np.array(M_)
-
-
-
-
-F1_ = []   # Arrays des F_alpha
-F2_ = []
+# choix des composantes f1 et f2 de f sous forme séparée
+# une seule composante tensorielle est retenue
+def f1(x):
+	return x*(1-x)
+def f2(y):
+	return y*(1-y)
 
 
 
-for i in range(I+1):
+F1 = np.zeros((gridsize+1,))
+F2 = np.zeros((gridsize+1,))
+
+for i in range(gridsize+1):
 	def produit1(x):
-		return f1(x) * les_phi[i](x)
+		return f1(x)*phi(x,i)
 	def produit2(x):
-		return f2(x) * les_phi[i](x)
+		return f2(x)*phi(x,i)
 
-	F1_.append(quad_trapz(produit1,0,1,p = 5))
-	F2_.append(quad_trapz(produit2,0,1,p = 5))
+	F1[i] = quad_trapz(produit1,0,1)
+	F2[i] = quad_trapz(produit2,0,1)
 
-
-F1 = np.array(F1_) # Matrices colonnes F_alpha
-F2 = np.array(F2_)
-
-
-
-# Quelques outils de calculs de matrice en chaine 
-def produit_matriciel4(A,B,C,D): # produit de 4 matrices
-	return np.dot(np.dot(A,B),np.dot(C,D)) # il y'a potentiellement un calcul matriciel plus efficace mais on ne va pas s'en soucier pour l'instant
-
-def produit_matriciel5(A,B,C,D,E): # prodiit de 5 matrices
-	return np.dot(produit_matriciel4(A,B,C,D),E)
-
-
+@numba.jit
 def A(V):   # A(V) est l'application M(V) dans l'énoncé
-	return np.dot(np.dot(V.T,np.dot(D,V)),M) + np.dot(np.dot(V.T,np.dot(M,V)),D) + np.dot(np.dot(V.T,np.dot(M,V)),M)
+	return np.dot(np.dot(V.T,np.dot(rigidity_matrix,V)),mass_matrix) + np.dot(np.dot(V.T,np.dot(mass_matrix,V)),rigidity_matrix) + np.dot(np.dot(V.T,np.dot(mass_matrix,V)),mass_matrix)
+
+def F(V,F1,F2,Rlist,Slist):
+	VTD = np.dot(V.T,rigidity_matrix)
+	VTM = np.dot(V.T,mass_matrix)
+
+	# Calcule un nouveau terme de la somme
+	def Fterm(V,R,S):
+			return np.dot(np.dot(VTD,S),np.dot(mass_matrix,R)) +\
+				np.dot(np.dot(VTM,S),np.dot(rigidity_matrix,R)) +\
+				np.dot(np.dot(VTM,S),np.dot(mass_matrix,R))
+
+	dim = gridsize + 1
+	res = np.dot(np.dot(V.T,F2),F1)
+	n = len(Rlist)
+	for k in range(1,n):
+		res -= Fterm(V,Rlist[k],Slist[k])
+	return res
+
+def G(V,F1,F2,Rlist,Slist):
+	return F(V,F2,F1,Slist,Rlist)
+
+# Méthode de point fixe; part des matrices aléatoires initialisées
+def fixed_point(F1,F2,Rlist,Slist,m):
+	S = np.array(np.random.rand(gridsize+1,)) # So est choisie aléatoirement
+	R = S # R est ainsi initialisée
+	for _ in range(m):
+		R_prev = R
+		S_prev = S
+
+		R = np.dot(np.linalg.inv(A(S)),F(S,F1,F2,Rlist,Slist))
+		S = np.dot(np.linalg.inv(A(R)),G(R,F1,F2,Rlist,Slist))
+
+	# La précision est la taille de la différence entre les deux dernières itérées
+	prec = max(np.linalg.norm(R-R_prev),np.linalg.norm(S-S_prev))
+	return R,S,prec
+
+def run():
+	Rlist = []
+	Slist = []
+
+	m = 20
+	n = 10
+	for i in range(n+1):
+		R,S,prec = fixed_point(F1,F2,Rlist,Slist,m)
+		print("Précision du point fixe",i,":",prec)
+		Rlist.append(R)
+		Slist.append(S)
 
 
-def F(V):
-	return np.dot(np.dot(V.T,F2),F1) 
+	return Rlist,Slist
+
+## Construction de la solution
+def build_sol(run_results):
+	Rlist,Slist = run_results
+	dim = gridsize + 1
+	steps = len(Rlist)
+
+	# Construit la fonction somme(g_i*phi_i)
+	def build_base(x,G):
+		return np.sum((g*phi(x,i) for i,g in enumerate(G)))
+
+	def u(x,y):
+		somme = np.sum((build_base(x,Rlist[n])*build_base(y,Slist[n]) for n in range(steps)))
+		return somme
+
+	return u
 
 
-def G(V):
-	return np.dot(np.dot(V.T,F1),F2)
+def graphe_sol():
+	fig = plt.figure(0)
+	ax0 = fig.add_subplot(221)
+	ax0.grid(True)
+	ax1 = fig.add_subplot(222,projection='3d')
+	ax1.grid(True)
+	ax2 = fig.add_subplot(223)
+	ax2.grid(True)
+	ax3 = fig.add_subplot(224,projection='3d')
+	ax3.grid(True)
 
+	intervalle = np.linspace(0,1,gridsize,endpoint=True)
+	domain = np.meshgrid(intervalle,intervalle)
 
-# Algorithme récurisif pour le calcul des vecteurs colonnes R et S avec n et m fixés 
+	solFunction = build_sol(run())
+	solVals = solFunction(*domain)
 
-S0 = np.array(np.random.rand(I+1,)) # So est choisie aléatoirement
-R0 = S0 # R0 est ainsi initialisée
-Fval = F(S0)
-Gval = G(R0)
-print A(S0)
-i = 0 
-while(i <= n):
-	
-	S0_before = S0
-	R0_before = R0
-	k = 0
-	while(k <= m):
-		R0 = np.dot(np.linalg.inv(A(S0)),F(S0))
-		S0 = np.dot(np.linalg.inv(A(R0)),G(R0))
-		R0 = np.dot(np.linalg.inv(A(S0)),F(S0))
-		k += 1
-	
-	Fval = Fval - produit_matriciel5(S0.T,D,S0_before,M,R0_before) - produit_matriciel5(S0.T,M,S0_before,D,R0_before) - produit_matriciel5(S0.T,M,S0_before,M,R0_before)
-	Gval = Gval - produit_matriciel5(R0.T,D,R0_before,M,S0_before) - produit_matriciel5(R0.T,M,R0_before,D,S0_before) - produit_matriciel5(R0.T,M,R0_before,M,S0_before)
-	i+=1
+	fVals = (lambda x,y: f1(x)*f2(y))(*domain)
 
+	ax0.imshow(solVals,extent=[0,1,0,1])
+	ax1.plot_wireframe(*domain,solVals)
+	ax2.imshow(fVals,extent=[0,1,0,1])
+	ax3.plot_wireframe(*domain,fVals)
 
+	return fig
 
-print ("ok")
+fig = graphe_sol()
+plt.show()
